cf9125c47c405031788d3799db99e6b5eaad0ac6
judifer <45107199+judifer@users.noreply.github.com>
2021-12-06 07:09:37 +0100

    Day 6 in python

---
+++ day06.py
@@ -0,0 +1,30 @@
+with open("day6.txt") as f:
+    data = f.read()
+
+fish = list(map(int, data.split(",")))
+
+breed = dict()
+for i in range(0, 10):
+    breed[i] = 0
+    
+for i in fish:
+    breed[i] += 1
+
+x = 0
+
+while x < 256:
+    for fi in range(0, 10):
+        if fi == 0:
+            breed[9] += breed[fi]
+            breed[7] += breed[fi]
+            breed[fi] = 0
+        else:
+            breed[fi - 1] += breed[fi]
+            breed[fi] = 0
+    x += 1
+
+result = 0
+for fi in range(0, 10):
+    result += breed[fi]
+
+print(result)
