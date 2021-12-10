6940124bce59ce57e3962e4e170fe332b95a82b5
judifer <45107199+judifer@users.noreply.github.com>
2021-12-10 08:04:27 +0100

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,50 @@
+with open("day10.txt") as f:
+    data = [x.strip() for x in f.readlines()]
+
+points = {")": 3, "]": 57, "}": 1197, ">": 25137}
+opening = ["(", "[", "{", "<"]
+closing = [")", "]", "}", ">"]
+pairs = {"(": ")", "[": "]", "{": "}", "<": ">"}
+
+incomplete_lines = list()
+
+score = 0
+
+for i in data:
+    instruction = list()
+    for j in i:
+        if j in opening:
+            instruction.append(j)
+        elif j in closing:
+            if j == ")" and instruction[-1] == "(":
+                instruction.pop()
+            elif j == "]" and instruction[-1] == "[":
+                instruction.pop()
+            elif j == "}" and instruction[-1] == "{":
+                instruction.pop()
+            elif j == ">" and instruction[-1] == "<":
+                instruction.pop()
+            else:
+                score += points[j]
+                break
+    else:
+        incomplete_lines.append(instruction)
+
+
+print("Part 1:", score)
+
+points = {"(": 1, "[": 2, "{": 3, "<": 4}
+
+score = 0
+scores = list()
+for i in incomplete_lines:
+    score = 0
+    for j in i[::-1]:
+        score *= 5
+        score += points[j]
+    scores.append(score)
+
+scores.sort()
+score = scores[(len(scores)) // 2]
+
+print("Part 2:", score)
