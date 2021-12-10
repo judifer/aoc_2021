7a8d36b5ae31b33a68c7b707db5c153bc5e0a462
judifer <45107199+judifer@users.noreply.github.com>
2021-12-10 22:31:30 +0100

    Update day10_vis.py

--- day10_vis.py
+++ day10_vis.py
@@ -8,17 +8,9 @@ opening = ["(", "[", "{", "<"]
 closing = [")", "]", "}", ">"]
 pairs = {"(": ")", "[": "]", "{": "}", "<": ">"}
 
-incomplete_lines = list()
+to_print = list()
 
-to_print = dict()
-
-
-score = 0
-corrupt_lines = dict()
-score2 = 0
-scores = list()
 for idx, i in enumerate(data):
-    score2 = 0
     corrupt = False
     instruction = list()
     for j in i:
@@ -34,40 +26,26 @@ for idx, i in enumerate(data):
             elif j == ">" and instruction[-1] == "<":
                 instruction.pop()
             else:
-                score += points[j]
                 corrupt = True
-                to_print[i] = j
+                a = f"{i}{' ' * (110 - len(i))}Corrupt! Unexpected \'{j}\'{' ' * 20}"
+                to_print.append(a)
                 break
     if corrupt == False:
         chars = list()
         for a in instruction[::-1]:
-            score2 *= 5
-            score2 += points[a]
             chars.append(pairs[a])
-        scores.append(score2)
-        characters = "".join(chars)
-        ins = "".join(instruction)
-        to_print[ins] = characters
+        a = f"{i}{' ' * (110 - len(i))}Incomplete! Complete with {''.join(chars)}"
+        to_print.append(a)
 
-print_lines = list()
-
-for i in to_print.keys():
-    if len(to_print[i]) > 1:
-        a = f"{i}{' ' * (110 - len(i))}Incomplete! Complete with {to_print[i]}"
-        print_lines.append(a)
-    else:
-        a = f"{i}{' ' * (110 - len(i))}Corrupt! Unexpected \'{to_print[i]}\'{' ' * 20}"
-        print_lines.append(a)
-
-for num in range(0, len(print_lines)):
+for num in range(0, len(to_print)):
     stdout = sys.stdout
     with open(f"img/img{num:03d}.txt", "w") as f:
         sys.stdout = f
         if num > 30:
             for line in range(num - 30, num):
-                print(print_lines[line])
+                print(to_print[num])
         else:
             for line in range(0, num):
-                print(print_lines[line])
+                print(to_print[num])
             print("\n" * (29 - num))
     sys.stdout = stdout
