70eb491d4e94d44b51d3221f13316c41ac3d250b
judifer <45107199+judifer@users.noreply.github.com>
2021-12-12 11:22:38 +0100

    updated day 12 visualization

--- day12_vis.py
+++ day12_vis.py
@@ -17,16 +17,16 @@ graph = nx.DiGraph()
 
 for i in neighbours.keys():
     if i == "start":
-        graph.add_node(i, color="green")
+        graph.add_node(i)
     elif i == "end":
-        graph.add_node(i, color="red")
+        graph.add_node(i)
     else:
-        graph.add_node(i, color="blue")
+        graph.add_node(i)
 
 for i in neighbours.keys():
     for j in neighbours[i]:
         graph.add_edge(i, j)
 
-color_map = ['red' if node == 'end' else 'green' if node == 'start' else 'blue' for node in graph]        
-nx.draw(graph, with_labels=True, node_color=color_map)
+color_map = ['red' if node == 'end' else 'green' if node == 'start' else 'grey' for node in graph]
+nx.draw(graph, with_labels=True, node_size=1000, node_color=color_map, node_shape="h")
 plt.savefig("Visualizations/Day12_Visualization.png")
